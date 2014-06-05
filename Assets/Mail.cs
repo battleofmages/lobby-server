1c69fc362ef2da21de12ca7ef016212f8656af89
Eduard Urbach <e.urbach@gmail.com>
2014-06-05 15:24:30 +0200

    Mail configuration loaded from a file now

--- Assets/Mail.cs
+++ Assets/Mail.cs
@@ -1,12 +1,13 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 
 public class Mail {
+	public static string smtpServer = "smtp.gmail.com";
+	public static string smtpUser;
+	public static string smtpPassword;
+	
 	public static void Send(string to, string subject, string body, string mailFrom = "noreply@battleofmages.com") {
 		MailMessage mail = new MailMessage();
 		
@@ -15,19 +16,19 @@ public class Mail {
 		mail.Subject = subject;
 		mail.Body = body;
 		
-		SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
-		smtpServer.Port = 587; // 465
-		smtpServer.Credentials = new System.Net.NetworkCredential("battleofmages@gmail.com", "bomlobbymail") as ICredentialsByHost;
-		smtpServer.EnableSsl = true;
-		smtpServer.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
+		SmtpClient smtpClient = new SmtpClient(smtpServer);
+		smtpClient.Port = 587; // 465
+		smtpClient.Credentials = new NetworkCredential(smtpUser, smtpPassword) as ICredentialsByHost;
+		smtpClient.EnableSsl = true;
+		smtpClient.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
 		ServicePointManager.ServerCertificateValidationCallback = delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
 			return true;
 		};
 		
 		try {
-			smtpServer.SendAsync(mail, to);
+			smtpClient.SendAsync(mail, to);
 		} catch(SmtpException e) {
-			LogManager.General.LogError("Could not send mail message to: '" + to + "' (" + e.ToString() + ")");
+			LogManager.General.LogError("Could not send mail message to: '" + to + "' (" + e + ")");
 		}
 	}
 	
@@ -41,7 +42,7 @@ public class Mail {
 		}
 		
 		if(e.Error != null) {
-			LogManager.General.LogError(string.Format("[{0}] E-Mail send error: {1}", token, e.Error.ToString()));
+			LogManager.General.LogError(string.Format("[{0}] E-Mail send error: {1}", token, e.Error));
 		} else {
 			LogManager.General.Log(string.Format("Sent activation mail to '{0}'", token));
 		}
